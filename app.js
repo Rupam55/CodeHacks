782ba3b3743b137ad5d0ef8295e56c787863a218
rupam karmakar <rupamkarmakar55@gmail.com>
2020-08-02 18:55:46 +0530

    r_practice-done

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-//const _ = require("lodash");
+// const _ = require("lodash");
 const request = require('request');
 
 const app = express();
@@ -11,27 +13,82 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// custom variable
+
+var usern = "";
+var rating = "";
+var rank = "";
+
+// get route
+
 app.get('/', function (req, res) {
     res.render('login');
 })
 
 app.get("/homepage", function (req, res) {
-    request('https://codeforces.com/api/problemset.problems?tags=dp', function (error, response, body) {
+    usern = req.query.username;
+    console.log(usern);
+    request('https://codeforces.com/api/user.info?handles='+usern, function (error, response, body) {
         if (error) {
-            console.log("SOmething Wnt Wrong!!");
+            alert("something went wrong");
             console.log(error);
         }
         else {
             if (response.statusCode == 200) {
-                //console.log(body);
-                var data = JSON.parse(body);
-                res.render('dashboard', { data: data["result"]["problems"] });
-                //console.log(parsedData["result"]["problems"][0]);
+                var userdata = JSON.parse(body);
+                usern = userdata["result"][0]["handle"];
+                rating = userdata["result"][0]["rating"];
+                rank = userdata["result"][0]["rank"];
+                // console.log(usern,rating,rank);
+                var newdata = [];
+                res.render('dashboard', { data: newdata , usern : usern , rating : rating , rank : rank });
             }
         }
     })
-    //res.send("login");
 });
+
+app.get("/compete", function (req, res) {
+    var newdata = [];
+    console.log(newdata.length);
+    res.render('compete', { data: newdata });
+});
+
+
+// post route
+
+
+app.post("/homepage", function (req, res) {
+    const tags = req.body.tags;
+    const rating = req.body.rating;
+    console.log(tags,rating);
+    
+    request('https://codeforces.com/api/problemset.problems?tags='+tags, function (error, response, body) {
+        if (error) {
+            alert("something went wrong");
+            console.log(error);
+        }
+        else {
+            if (response.statusCode == 200) {
+                var data = JSON.parse(body);
+                var i=0,y=0;
+                var newdata = [];
+                while(i<6){
+                    if(data["result"]["problems"][y]["rating"] < rating && data["result"]["problems"][y]["rating"] >= rating - 200){  
+                        i++;
+                        newdata.push(data["result"]["problems"][y]);
+                        console.log(data["result"]["problems"][y]["rating"]);
+                    }
+                    y++;
+                }
+                console.log(newdata.length);
+                res.render('dashboard', { data: newdata , usern : usern , rating : rating , rank : rank });
+            }
+        }
+    })
+});
+
+
+// listen route
 
 const PORT = process.env.PORT || 3000;
 
